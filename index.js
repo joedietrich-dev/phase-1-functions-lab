5e2989bd2f08955057d50f8ae3f87ac77013bb67
Joe Dietrich <joe@joedietrich.net>
2021-06-29 22:20:18 -0400

    commit tree 648edf53f4dbb319964f572fd1599206b03d3ea6

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+function calculatesFarePrice(start, end) {
+  const feet = distanceTravelledInFeet(start, end);
+  const fare = feet <= 400 ? 0
+    : feet <= 2000 ? ((feet - 400) * 2) / 100
+      : feet <= 2500 ? 25
+        : 'cannot travel that far';
+  return fare;
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(start - end) * 264;
+}
+
+function distanceFromHqInFeet(end) {
+  return distanceTravelledInFeet(42, end);
+}
+
+function distanceFromHqInBlocks(end) {
+  return Math.abs(42 - end);
+}
